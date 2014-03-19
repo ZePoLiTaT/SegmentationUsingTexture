ace23b98bebb505b55ca8b32709f17c33ec65c5c
Tatiana L <zepolitat@sirius.utp.edu.co>
2014-03-19 15:01:57 +0100

    Issue #12: Modified algorithm of region growing for N dimensions Added normalization of local features Still Pending: Check error and execution time

--- lab2.m
+++ lab2.m
@@ -1,32 +1,92 @@
-clear; clc; close all;
+ function lab2
 
-a =[0 2 0 2 0
-    0 2 0 2 0
-    0 2 0 2 0
-    1 1 1 3 1
-    1 1 1 3 1];
+    clear; clc; close all;
 
-b =[2 2 2 2 2
-    2 2 2 2 2
-    2 2 2 2 2
-    1 1 1 1 1
-    1 1 1 1 1];
+    %test_segmentation_toy
+    %test_segmentation;
+    
+    test_classification;
 
-comat_a = graycomatrix(a, 'Offset',[0 1], 'NumLevels',4, 'GrayLimits', [0 3], 'Symmetric', true)
-comat_b = graycomatrix(b, 'Offset',[0 1], 'NumLevels',4, 'GrayLimits', [0 3], 'Symmetric', true)
+ end
+
+ function test_segmentation_toy
+    % Toy image:
+%    im = zeros(5,4,3);
+%     im(:,:,1) = [ 50 45 100 101;
+%                   49 48 105 104;
+%                   200 47 2 3;
+%                   100 5 1 4;
+%                   101 102 10 300];
+%               
+%     im(:,:,2) = [ 50 45 100 101;
+%                   49 48 105 104;
+%                   200 47 2 3;
+%                   100 5 1 4;
+%                   101 102 10 300];
+%               
+%     im(:,:,3) = [ 50 45 100 101;
+%                   49 48 105 104;
+%                   200 47 2 3;
+%                   100 5 1 4;
+%                   101 102 10 300];
+              
+    %Homogeneous image
+    im = ones(6,7,3);
+    im(4:6,:,:) = 120
+
+    tic
+    toy_features = texture_features_local(im, 3, 0, 1);
+    toc
+ end
+ 
+ function test_segmentation
+ 
+    im_feli = imread('P2_seg/feli.tif');
+    im_feli_gray = rgb2gray( im_feli );
+    
+    tic
+    im_feli_features = texture_features_local(im_feli, 11, 0,4);
+    toc
+    %features = nlfilter(im_feli_gray,[11 11],@get_texture_features);
+    
+    threshold = 10;
+    
+    % Region growing for color images without filter
+    [imlabels, lbl_stats] = region_growing(im_feli_features, threshold);
+    
+ end
+ 
+ function test_classification()
+    im_feli = imread('P2_seg/feli.tif');
+    im_feli = rgb2gray(im_feli);
+    comat_im_feli = graycomatrix(im_feli, 'Offset',[0 2], 'Symmetric', true);
+    stats_im_feli = graycoprops(comat_im_feli,{'Contrast','Homogeneity', 'Energy', 'Correlation'})
+ end
+ 
+ function test_comatrices()
+    a =[0 2 0 2 0
+        0 2 0 2 0
+        0 2 0 2 0
+        1 1 1 3 1
+        1 1 1 3 1];
+
+    b =[2 2 2 2 2
+        2 2 2 2 2
+        2 2 2 2 2
+        1 1 1 1 1
+        1 1 1 1 1];
+
+    comat_a = graycomatrix(a, 'Offset',[0 1], 'NumLevels',4, 'GrayLimits', [0 3], 'Symmetric', true)
+    comat_b = graycomatrix(b, 'Offset',[0 1], 'NumLevels',4, 'GrayLimits', [0 3], 'Symmetric', true)
 
 
-stats_a = graycoprops(comat_a,{'Contrast','Homogeneity', 'Energy', 'Correlation'})
-stats_b = graycoprops(comat_b,{'Contrast','Homogeneity', 'Energy', 'Correlation'})
+    stats_a = graycoprops(comat_a,{'Contrast','Homogeneity', 'Energy', 'Correlation'})
+    stats_b = graycoprops(comat_b,{'Contrast','Homogeneity', 'Energy', 'Correlation'})
 
-
-
-im_feli = imread('P2_seg/feli.tif');
-im_feli = rgb2gray(im_feli);
-comat_im_feli = graycomatrix(im_feli, 'Offset',[0 2], 'Symmetric', true);
-stats_im_feli = graycoprops(comat_im_feli,{'Contrast','Homogeneity', 'Energy', 'Correlation'})
-
-
-fun = @(x) graycomatrix(x, 'Offset',[0 1], 'Symmetric', true);
-features = nlfilter(im_feli,[3 3],fun);
-
+ end
+ 
+function stats_x = get_texture_features(x)
+    comat_x = graycomatrix(x, 'Offset',[0 1], 'Symmetric', true);
+    stats_x = graycoprops(comat_x,{'Contrast'});
+    stats_x = stats_x.Contrast;
+end
--- texture_features_local.m
+++ texture_features_local.m
@@ -1,10 +1,72 @@
-function [ features ] = texture_features_local( im, wsize )
-%UNTITLED3 Summary of this function goes here
+function [ features ] = texture_features_local( im, wsize, theta, d )
+% TEXTURE_FEATURES_LOCAL Computes a feature vector per pixel composed by the
+%texture features given by the statistics of the 
 %   Detailed explanation goes here
 
+    imgray = rgb2gray(im);
 
-    fun = @(x) graycomatrix(x, 'Offset',[0 1], 'NumLevels',4, 'GrayLimits', [0 3], 'Symmetric', true);
-    features = nlfilter(im,[3 3],fun);
-
+    [R,C,~] = size( im );
+    wsize_2 = floor(wsize/2);
+    
+    pad_r = wsize_2+1 : R - wsize_2;
+    pad_c = wsize_2+1 : C - wsize_2;
+    
+    %The resulting feature matrix will have 7 channels:
+    %   1. Contrast
+    %   2. Homogen  eity
+    %   3. Energy
+    %   4. Correlation
+    %   5. R channel
+    %   6. G channel
+    %   7. B channel
+    features = zeros(R,C, 5);
+    
+    for i = pad_r
+        for j = pad_c
+            submat = imgray(i-wsize_2:i+wsize_2, j-wsize_2:j+wsize_2, :);
+            
+            comat_submat = graycomatrix(submat, 'Offset',[theta d], 'Symmetric', true);
+            stats_submat = graycoprops(comat_submat,{'Contrast','Homogeneity', 'Energy', 'Correlation'});
+            
+            features(i,j,1) = stats_submat.Contrast;      %Contrast
+            features(i,j,2) = stats_submat.Homogeneity;   %Homogeneity
+            %features(i,j,3) = stats_submat.Energy;        %Energy
+            %features(i,j,4) = stats_submat.Correlation;   %Correlation
+            features(i,j,3) = im(i,j,1);                  %R
+            features(i,j,4) = im(i,j,2);                  %G
+            features(i,j,5) = im(i,j,3);                  %B 
+        end
+    end
+        
+    %Normalize at the end!!
+    features(pad_r, pad_c, :) = normalize_features( features(pad_r, pad_c, :) )
+    
 end
 
+function norm_features = normalize_features( features )
+%% NORM_FEATURES Normalizes the feature matrix 
+%   NORM_FEATURES(features) returns a normalized version of the features
+%   where the mean value of each feature is 0 and the standard deviation is
+%   one.
+
+    [R,C,F] = size(features);
+    features2D = double( unfold_matrix( features ) );
+    
+    mean_f = mean(features2D);
+    norm_features = bsxfun( @minus, features2D, mean_f );
+    
+    std_f = std(features2D);
+    norm_features = bsxfun( @rdivide, norm_features, std_f );
+    
+    norm_features = fold_matrix(norm_features,R,C,F);
+end
+
+function mat2D = unfold_matrix(mat3D)
+
+    [R,C,F] = size(mat3D);
+    mat2D = reshape( mat3D, C*R, F );
+end
+
+function mat3D = fold_matrix(mat2D,R,C,F)
+    mat3D = reshape( mat2D, R, C, F );
+end
