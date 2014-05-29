555270a18f2a31710233a07c2338f469b9735390
evargasv <liz2102@gmail.com>
2014-05-29 15:29:46 +0200

    Final version of the source code and the report

--- texture_features_local.m
+++ texture_features_local.m
@@ -1,72 +1,43 @@
-function [ features ] = texture_features_local( im, wsize, theta, d )
+function [ features, fSize ] = texture_features_local( imgray, wsize, d, numlvl )
 % TEXTURE_FEATURES_LOCAL Computes a feature vector per pixel composed by the
-%texture features given by the statistics of the 
-%   Detailed explanation goes here
+% texture features given by the statistics of the co-occurrence matrix
 
-    imgray = rgb2gray(im);
+    if (size(imgray,3) > 1)
+        imgray = rgb2gray(imgray);
+    end
 
-    [R,C,~] = size( im );
     wsize_2 = floor(wsize/2);
+    imgray = padarray(imgray,[wsize_2 wsize_2]);
+    
+    [R,C,~] = size( imgray );
     
     pad_r = wsize_2+1 : R - wsize_2;
-    pad_c = wsize_2+1 : C - wsize_2;
+    fSize(1) = length(pad_r);
     
-    %The resulting feature matrix will have 7 channels:
+    pad_c = wsize_2+1 : C - wsize_2;
+    fSize(2) = length(pad_c);
+    
+    fSize(3) = 3*size(d,1);
+    
+    %The resulting feature matrix will have 3 x (dir,ang) channels:
     %   1. Contrast
     %   2. Homogen  eity
     %   3. Energy
-    %   4. Correlation
-    %   5. R channel
-    %   6. G channel
-    %   7. B channel
-    features = zeros(R,C, 5);
+    features = zeros( fSize(1), fSize(2) , fSize(3) );
     
     for i = pad_r
         for j = pad_c
-            submat = imgray(i-wsize_2:i+wsize_2, j-wsize_2:j+wsize_2, :);
+        
+            submat = imgray(i-wsize_2:i+wsize_2, j-wsize_2:j+wsize_2);
             
-            comat_submat = graycomatrix(submat, 'Offset',[theta d], 'Symmetric', true);
-            stats_submat = graycoprops(comat_submat,{'Contrast','Homogeneity', 'Energy', 'Correlation'});
+            comat_submat = graycomatrix(submat, 'Offset',d, 'NumLevels',numlvl, 'Symmetric', true );
+            stats_submat = graycoprops(comat_submat,{'Contrast','Homogeneity','Energy'});
             
-            features(i,j,1) = stats_submat.Contrast;      %Contrast
-            features(i,j,2) = stats_submat.Homogeneity;   %Homogeneity
-            %features(i,j,3) = stats_submat.Energy;        %Energy
-            %features(i,j,4) = stats_submat.Correlation;   %Correlation
-            features(i,j,3) = im(i,j,1);                  %R
-            features(i,j,4) = im(i,j,2);                  %G
-            features(i,j,5) = im(i,j,3);                  %B 
+            for k=0: size(d,1)-1
+                features(i-wsize_2,j-wsize_2,1 + 3*k) = stats_submat.Contrast(k+1);      %Contrast
+                features(i-wsize_2,j-wsize_2,2 + 3*k) = stats_submat.Homogeneity(k+1);   %Homogeneity
+                features(i-wsize_2,j-wsize_2,3 + 3*k) = stats_submat.Energy(k+1);        %Energy
+            end
         end
     end
-        
-    %Normalize at the end!!
-    features(pad_r, pad_c, :) = normalize_features( features(pad_r, pad_c, :) )
-    
-end
-
-function norm_features = normalize_features( features )
-%% NORM_FEATURES Normalizes the feature matrix 
-%   NORM_FEATURES(features) returns a normalized version of the features
-%   where the mean value of each feature is 0 and the standard deviation is
-%   one.
-
-    [R,C,F] = size(features);
-    features2D = double( unfold_matrix( features ) );
-    
-    mean_f = mean(features2D);
-    norm_features = bsxfun( @minus, features2D, mean_f );
-    
-    std_f = std(features2D);
-    norm_features = bsxfun( @rdivide, norm_features, std_f );
-    
-    norm_features = fold_matrix(norm_features,R,C,F);
-end
-
-function mat2D = unfold_matrix(mat3D)
-
-    [R,C,F] = size(mat3D);
-    mat2D = reshape( mat3D, C*R, F );
-end
-
-function mat3D = fold_matrix(mat2D,R,C,F)
-    mat3D = reshape( mat2D, R, C, F );
 end
